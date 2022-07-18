44c87cdcdd7ea0a4740f2e323bf562eff8fbcaeb
Aqib Raza <aqibraza@outlook.com>
2022-07-19 04:40:26 +0500

    added routing and fixed css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-	return <div className="App">App component</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/Signup" element={<Signup />} />
+
+				{/* If route does not match, show the following. */}
+				<Route
+					path="*"
+					element={
+						<p
+							style={{
+								textAlign: "center",
+								marginTop: "5rem",
+								fontSize: "1.3125rem",
+							}}
+						>
+							404: Nothing found at this link.
+						</p>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
