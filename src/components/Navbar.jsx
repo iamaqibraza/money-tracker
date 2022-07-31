102ecb1ef07237eed79b3da85c4ef110a8826764
Aqib Raza <aqibraza@outlook.com>
2022-07-31 10:11:44 +0500

    changed feedback button position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,31 +11,11 @@ export default function Navbar() {
 		logout,
 	} = useLogout();
 	return (
-		<div className="bg-gray-800 py-4 flex justify-between items-center px-4 md:px-16 lg:px-28 xl:px-44 ">
+		<div className="bg-gray-800 py-4 flex justify-between items-center px-4 md:px-16 lg:px-28 xl:px-44">
 			<div className="flex items-center">
 				<h1 className="flex justify-between font-bold text-lg text-white sm:text-2xl mr-4">
 					<Link to="/">Money Tracker</Link>
 				</h1>
-				{user && (
-					<Tooltip tooltipMessage="Feedback">
-						<Link to="/feedback">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-6 w-6 text-gray-100 cursor-pointer"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								strokeWidth={1.5}
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-								/>
-							</svg>
-						</Link>
-					</Tooltip>
-				)}
 			</div>
 
 			<div className="flex space-x-4 items-center">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 import TransactionForm from "../../components/TransactionForm";
 import TransactionList from "../../components/TransactionList";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import useFirestoreCollection from "../../hooks/useFirestoreCollection";
+import { Tooltip } from "../../components/Tooltip";
 export default function Home() {
 	const { user } = useAuthContext();
 	const {
@@ -14,17 +16,41 @@ export default function Home() {
 		["createdAt"]
 	);
 	return (
-		<div className="flex flex-col items-center mt-16 md:flex-row md:items-start justify-between mx-auto px-4 md:px-16 lg:px-28 xl:px-44">
-			<TransactionForm />
-			<div className="md:order-first mt-16 md:mt-0 w-full">
-				{error && <h3>{error}</h3>}
-				{transactions && (
-					<TransactionList
-						transactions={transactions}
-						isPending={isPending}
-					/>
-				)}
+		<>
+			{user && (
+				<div className="fixed bottom-10 left-0 flex justify-end pt-8 px-4 md:px-16 lg:px-28 xl:px-44">
+					<Tooltip tooltipMessage="Feedback">
+						<Link to="/feedback">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-11 w-11 p-2 text-gray-100 cursor-pointer bg-white/10 rounded-full"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								strokeWidth={1.5}
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+								/>
+							</svg>
+						</Link>
+					</Tooltip>
+				</div>
+			)}
+			<div className="flex flex-col items-center mt-16 md:flex-row md:items-start justify-between mx-auto px-4 md:px-16 lg:px-28 xl:px-44">
+				<TransactionForm />
+				<div className="md:order-first mt-16 md:mt-0 w-full">
+					{error && <h3>{error}</h3>}
+					{transactions && (
+						<TransactionList
+							transactions={transactions}
+							isPending={isPending}
+						/>
+					)}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
